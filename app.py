43079f85966cee43c6c0cbb10efca0b1e72506b9
Jonathan Laurent <jlaurent4@student.gsu.edu>
2022-02-24 22:15:34 -0500

    initial commit

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import find_dotenv, load_dotenv
+import random
+
 
 load_dotenv(find_dotenv())
 
@@ -15,32 +17,48 @@ db = SQLAlchemy(app)
 class Shows(db.Model):
     """class person"""
     id = db.Column(db.Integer, primary_key=True)
-    tvshow = db.Column(db.String(80), unique=True, nullable=False)
+    comment = db.Column(db.String(80), unique=False, nullable=False)
+    # rating = db.Column(db.String(80), unique=False, nullable=False)
+    movie_id = db.Column(db.String(80), unique=False, nullable=False)
+
 
 db.create_all()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     """landing page"""
+    external_ids = ["27205", "557", "419430", "496243"]
+    new_id = random.choice(external_ids)
     if flask.request.method == "POST":
         data = flask.request.form
-        add_show = data.get("addShow")
-        delete_show = data.get("deleteShow")
-        if add_show:
-            new_show = Shows(tvshow=add_show)
+        add_comment = data.get("comment")
+        # add_rating = data.get("rating")
+        if add_comment:
+            new_show = Shows(comment=add_comment, movie_id=new_id) #, rating=add_rating)
             db.session.add(new_show)
             db.session.commit()
-        elif delete_show:
-            if Shows.query.filter_by(tvshow=delete_show):
-                Shows.query.filter_by(tvshow=delete_show).delete()
-            db.session.commit()
-    data = Shows.query.all()
-    num_shows = len(data)
+
+    data = Shows.query.filter_by(movie_id=new_id).all()
+    num_data = len(data)
+    # use a for loop that cycles through all of this data and then access each part that I need.
+    # how can the page stop refreshing? 
     return flask.render_template(
         "index.html",
         data=data,
-        num_shows=num_shows,
+        movie_id=new_id,
+        num_data=num_data,
     )
+
+@app.route("/handle_form", methods=["POST"])
+def handle_form():
+    """handle page"""
+    data = flask.request.form
+    campus_id = data.get("CampusID")
+    if campus_id == "jlaurent4":
+        return flask.redirect(flask.url_for("welcome")) # inside the quotations are function names
+    else:
+        flask.flash("Wrong campus id entered")
+        return flask.redirect(flask.url_for("index")) # inside the quotations is a function name
 
 if __name__ == "__main__":
     app.run()
